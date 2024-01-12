dc3e60e0791cae6549b7bebdab803b98680aba7e
rfnasution01 <rivaifnasution@gmail.com>
2024-01-12 09:26:06 +0700

    fix: memperbaiki responsibility katalog layout header

--- src/app/katalog/component/katalogPageHeader.tsx
+++ src/app/katalog/component/katalogPageHeader.tsx
@@ -11,9 +11,9 @@ export function KatalogPageHeader({
   >;
 }) {
   return (
-    <div className="flex items-center lg:justify-between w-full gap-10">
+    <div className="flex items-center lg:justify-between w-full gap-10 mt-12 pt-4 lg:mt-0 lg:pt-0 md:mt-0 md:pt-0">
       <KatalogHeaderInfo stateCurrency={stateCurrency} />
-      <div className="hidden lg:block md:block">
+      <div>
         <KatalogHeaderCurrency
           stateCurrency={stateCurrency}
           setStateCurrency={setStateCurrency}
